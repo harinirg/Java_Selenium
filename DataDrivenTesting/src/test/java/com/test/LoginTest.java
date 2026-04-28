0d17242fa20557918ab1ef9f5811710d0d7703dc
Harini R G <harinirg26@gmail.com>
2026-04-28 10:35:28 +0530

    Logger,Listener is implemented.

--- DataDrivenTesting/src/test/java/com/test/LoginTest.java
+++ DataDrivenTesting/src/test/java/com/test/LoginTest.java
@@ -12,11 +12,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.utilities.ExcelUtil;
 
-
+@Listeners(TestListener.class)
 public class LoginTest {
     public static Logger log = LogManager.getLogger(LoginTest.class);
 	
